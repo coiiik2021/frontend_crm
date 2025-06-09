3e9b84da455e327153181727204e7fa62aa7cc16
Danhuynh224 <vdan2242004@gmail.com>
2025-06-09 08:11:37 +0700

    chỉnh sửa thêm bill_id vào InvoiceHomeBill.jsx

--- src/components/admin/bills/invoice/InvoiceHomeBill.jsx
+++ src/components/admin/bills/invoice/InvoiceHomeBill.jsx
@@ -1,15 +1,16 @@
 import { GetInvoicePdf } from "../../../../service/api.admin.service";
 import React, { useEffect, useState } from "react";
 
-const PdfViewer = (bill_id) => {
-  const id = bill_id?.id || "";
+const PdfViewer = ({ bill_id }) => {
+  const id = bill_id || "";
   const [pdfUrl, setPdfUrl] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    console.log("PDF Viewer mounted with bill_id:", id);
     const loadPdf = async () => {
       try {
-        const response = await GetInvoicePdf();
+        const response = await GetInvoicePdf(id);
         const blob = new Blob([response], { type: "application/pdf" });
         const url = URL.createObjectURL(blob);
         setPdfUrl(url);
